11f7a23f19214a73a49f1db0c076942ce4295485
Anna <ds_anna@bk.ru>
2022-05-03 19:22:08 +0300

    Выяснить является ли число чётным

---
+++ task3/Program.cs
@@ -0,0 +1,5 @@
+﻿// Выяснить является ли число чётным
+Console.Write("Введите число ");
+int namber = int.Parse(Console.ReadLine());
+if(namber % 2 == 0) Console.WriteLine("Введенное число четное.");
+else Console.WriteLine("Введенное число нечетное.");
