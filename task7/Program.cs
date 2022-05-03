c8fee4d63250ef02fa0c781ddb0b37c5326ab381
Anna <ds_anna@bk.ru>
2022-05-03 20:07:07 +0300

    Удалить вторую цифру трёхзначного числа

---
+++ task7/Program.cs
@@ -0,0 +1,7 @@
+﻿// Удалить вторую цифру трёхзначного числа
+
+Console.Write("Введите трехзначное число ");
+int namber = int.Parse(Console.ReadLine());
+int namber1 = namber / 100;
+int namber3 = namber % 10;
+Console.WriteLine($"Число c удаленной второй цифрой: {namber1}{namber3}");
