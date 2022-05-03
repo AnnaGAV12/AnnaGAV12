d050efda9c4718fd54c82d8ff37bb37191abc6f9
Anna <ds_anna@bk.ru>
2022-05-03 20:36:12 +0300

    Выяснить, кратно ли число заданному, если нет, вывести остаток.

---
+++ task8/Program.cs
@@ -0,0 +1,8 @@
+﻿// Выяснить, кратно ли число заданному, если нет, вывести остаток.
+
+Console.Write("Введите заданное число ");
+int namber1 = int.Parse(Console.ReadLine());
+Console.Write("Введите кратное число заданному ");
+int namber2 = int.Parse(Console.ReadLine());
+if (namber1 % namber2 == 0) Console.Write("Число действительно кратное");
+else Console.Write($"{namber1} не кратно числу {namber2}. Остаток = {namber1 % namber2}");
