c8c890c80f51955b36279b202b3ea0e446f0834e
Anna <ds_anna@bk.ru>
2022-05-03 19:43:24 +0300

    Показать вторую цифру трёхзначного числа

---
+++ task5/Program.cs
@@ -0,0 +1,6 @@
+﻿// Показать вторую цифру трёхзначного числа
+
+Console.Write("Введите трехзначное число ");
+int namberA = int.Parse(Console.ReadLine());
+int namberB = namberA / 10 % 10;
+Console.Write("Вторая цифра " + namberB);
