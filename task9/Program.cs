7b6e6ace7f72bb52d8a98753ae3d502481f3089f
Anna <ds_anna@bk.ru>
2022-05-03 21:47:41 +0300

    Найти третью цифру числа или сообщить, что её нет

---
+++ task9/Program.cs
@@ -0,0 +1,11 @@
+﻿// See Найти третью цифру числа или сообщить, что её нет
+Console.WriteLine("Введите число для нахождения третьей цифры ");
+int namber = int.Parse(Console.ReadLine());
+if (namber < 100) Console.Write("Третьей цифры нет ");
+if (namber > 99 || namber < 1000) Console.Write($"Третья цифра {namber % 10}");
+else
+{
+    while (namber > 999 )
+    namber = namber / 10;
+Console.WriteLine($"Третья цифра числа {(namber % 10)} ");
+}
