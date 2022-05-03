712c8ae96835fa36388c6222e6e966ae68fa7928
Anna <ds_anna@bk.ru>
2022-05-03 19:09:42 +0300

    По заданному номеру дня недели вывести его названиe

---
+++ task2/Program.cs
@@ -0,0 +1,36 @@
+﻿// По заданному номеру дня недели вывести его название
+
+Console.Write("Введите номер дня недели ");
+int namber = int.Parse(Console.ReadLine());
+if (namber == 1)
+{
+    Console.WriteLine("Понедельник");
+}
+if (namber == 2)
+{
+    Console.WriteLine("Вторник");
+}
+if (namber == 3)
+{
+    Console.WriteLine("Среда");
+}
+if (namber == 4)
+{
+    Console.WriteLine("Четверг");
+}
+if (namber == 5)
+{
+    Console.WriteLine("Пятница");
+}
+if (namber == 6)
+{
+    Console.WriteLine("Суббота");
+}
+if (namber == 7)
+{
+    Console.WriteLine("Воскресенье");
+}
+if (namber > 7 || namber <= 0)
+{
+    Console.WriteLine("По данному номеру дня недели нет");
+}
