e51a69e9b94b3284f620f05b9e3d1a6e7d99a436
Anna <ds_anna@bk.ru>
2022-05-03 19:53:48 +0300

    Показать наибольшую цифру числа из отрезка [10, 99].

---
+++ task6/Program.cs
@@ -0,0 +1,8 @@
+﻿// Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+
+Console.Write("Введите число из отрезка [10, 99] ");
+int namber = int.Parse(Console.ReadLine());
+int namber1 = namber / 10;
+int namber2 = namber % 10;
+if (namber1 > namber2) Console.Write("Наибольшая цифра числа = " + namber1);
+else Console.Write("Наибольшая цифра числа = " + namber2);
