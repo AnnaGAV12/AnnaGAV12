0d91f75ff477617769e7ddadc74a4827e1e00f85
Anna <ds_anna@bk.ru>
2022-05-03 19:31:51 +0300

    Показать четные числа от 1 до N

---
+++ task4/Program.cs
@@ -0,0 +1,11 @@
+﻿// Показать четные числа от 1 до N
+
+Console.Write("Введите число N ");
+int namberN = int.Parse(Console.ReadLine());
+int count = 2;
+Console.WriteLine($"Четные числа:");
+while (count <= namberN)
+{
+    Console.Write(count + " ");
+    count=count + 2;
+}
